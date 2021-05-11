613adc39ff3da428f3e4010294607742a3f646b5
Andrade Sampaio <andrade.sampaio@outlook.com>
2021-05-11 16:37:36 -0300

    Terraform GCP Basics

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.64.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/vagrant/lab-devops-cloud-andrade-6b654d40cf22.json")
+
+  project = "lab-devops-cloud-andrade"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
